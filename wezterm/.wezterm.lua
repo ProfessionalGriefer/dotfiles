e635f1e00304b06b96aac876d0601336692b0c73
Vincent Nahn <vincentnahn@gmail.com>
2025-01-13 09:32:13 +0100

    Changed window padding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,4 +10,6 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
 return config
