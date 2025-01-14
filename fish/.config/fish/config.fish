7baaa2c0a5fe3a365a37b4e2ceba6917c8336cf9
Vincent Nahn <vincentnahn@gmail.com>
2025-01-14 19:51:59 +0100

    Added bat

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,12 +2,12 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /Users/vincent/miniconda3/bin/conda
-    eval /Users/vincent/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    eval /Users/vincent/miniconda3/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/Users/vincent/miniconda3/etc/fish/conf.d/conda.fish"
         . "/Users/vincent/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/Users/vincent/miniconda3/bin" $PATH
+        set -x PATH /Users/vincent/miniconda3/bin $PATH
     end
 end
 # <<< conda initialize <<<
@@ -17,11 +17,13 @@ oh-my-posh init fish | source
 
 
 # pnpm
-set -gx PNPM_HOME "/Users/vincent/Library/pnpm"
+set -gx PNPM_HOME /Users/vincent/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
 # Created by `pipx` on 2024-12-10 23:19:10
 set PATH $PATH /Users/vincent/.local/bin
+
+fzf --fish | source
