f5cc3f358ed2ed8ec1d58e36cbc02fafc9b61678
Vincent Nahn <vincentnahn@gmail.com>
2025-01-14 09:08:24 +0100

    Added color scheme to wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,5 +11,6 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.color_scheme = "Catppuccin Macchiato"
 
 return config
