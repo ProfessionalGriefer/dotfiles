ba8a06cda676dce3b94b92c8b9d658ebd244c4d8
Vincent Nahn <vincentnahn@gmail.com>
2025-01-17 00:07:30 +0100

    Added transparent background

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- add gruvbox
+  { "catppuccin", opts = {
+    transparent_background = true,
+  } },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
