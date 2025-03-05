c0cd247378cadace3082d050b3d232d0ed3fdbc9
Vincent Nahn <vincentnahn@gmail.com>
2025-03-05 23:11:42 +0100

    Slight modification

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -43,11 +43,12 @@ return {
       return title
     end,
 
-    preferred_link_style = "markdown",
+    preferred_link_style = "wiki",
 
-    open_app_foreground = false,
+    open_app_foreground = true,
 
     disable_frontmatter = true,
+
     templates = {
       folder = "5 - Templates",
       date_format = "%Y-%m-%d",
@@ -55,25 +56,23 @@ return {
       -- A map for custom variables, the key should be the variable and the value a function
       substitutions = {},
     },
-    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
-    -- URL it will be ignored but you can customize this behavior here.
+
     ---@param url string
     follow_url_func = function(url)
-      -- Open the URL in the default web browser.
       vim.fn.jobstart({ "open", url }) -- Mac OS
       -- vim.fn.jobstart({"xdg-open", url})  -- linux
       -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
       -- vim.ui.open(url) -- need Neovim 0.10.0+
     end,
 
-    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an image
-    -- file it will be ignored but you can customize this behavior here.
+    -- Doesn't work!
     ---@param img string
     follow_img_func = function(img)
-      vim.fn.jobstart({ "qlmanage", "-p", img }) -- Mac OS quick look preview
+      vim.fn.jobstart({ "qlmanage", "-p", '"' .. img .. '"' }) -- Mac OS quick look preview
       -- vim.fn.jobstart({"xdg-open", url})  -- linux
       -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
     end,
+
     sort_by = "modified",
     sort_reversed = true,
     open_notes_in = "current",
@@ -170,6 +169,7 @@ return {
         desc = "Show all tags",
       },
       {
+        -- brew install pngpaste
         "<leader>op",
         "<CMD>ObsidianPasteImg<CR>",
         desc = "Paste image from clipboard",
